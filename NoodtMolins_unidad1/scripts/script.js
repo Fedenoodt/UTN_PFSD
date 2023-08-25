746f1239e39ade274f551d63410f3aab197fa7e9
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-08-25 18:08:28 -0300

    Add files via upload

---
+++ NoodtMolins_unidad1/scripts/script.js
@@ -0,0 +1,5 @@
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const seguro = document.getElementById('seg');
+
+console.log(`${nombre}, ${apellido}, ${seguro}.`)
