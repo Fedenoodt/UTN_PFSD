ac8b954f22903ae48f5e6071fce118ab6bf36061
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-08-29 01:25:25 -0300

    Add files via upload

--- NoodtMolins_unidad1/scripts/script.js
+++ NoodtMolins_unidad1/scripts/script.js
@@ -1,5 +1,14 @@
-const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
-const seguro = document.getElementById('seg');
-
-console.log(`${nombre}, ${apellido}, ${seguro}.`)
+function segurosValor () {
+    let txt;
+    let kind = document.getElementById('seg').value;
+    if (kind == 'basico') {
+        txt = '<h2>El seguro de tipo Básico cuesta $500 al mes.</h2>';
+    }
+    else if (kind == 'intermedio') {
+        txt = '<h2>El seguro de tipo Intermedio cuesta $1000 al mes.</h2>';
+    }
+    else if (kind == 'premium') {
+        txt = '<h2>El seguro de tipo Premium cuesta $1500 al mes.</h2>';
+    }
+    document.getElementById('txt').innerHTML = txt;
+}segurosValor()
